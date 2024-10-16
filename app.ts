53481d1f3de72a455ecc6de96667b95e73c5adbe
tajpuriya27 <tajpuriyasunil27@gmail.com>
2024-10-16 14:53:31 +0545

    init: working to initialize a node app using ts

---
+++ app.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+
+import middleware from "./utils/middleware";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+if (process.env.NODE_ENV === "test") {
+  const testingRouter = require("./controllers/testing");
+  app.use("/api/testing", testingRouter);
+}
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+export default app;
---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (_req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(4000, () => {
+  console.log(`Server running on port 4000`);
+});
